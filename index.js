b755b330af44dd7465ff40550458b5a3d3d1433a
Gray Hanna <ghostblossom54288@gmail.com>
2024-05-21 12:12:57 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// document.body.append(element);
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+//   element.append(ul);
+main.remove();
+    const newHeader = document.createElement("h1")
+    newHeader.id= 'victory'
+    newHeader.textContent='YOUR-NAME is the champion'
+
+    

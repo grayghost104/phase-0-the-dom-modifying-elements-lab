e9e0b339cb6553e79f881f486d0b8c309c14e861
grayghost104 <167383286+grayghost104@users.noreply.github.com>
2024-06-05 09:47:57 -0600

    commit

--- index.js
+++ index.js
@@ -9,6 +9,6 @@
 main.remove();
     const newHeader = document.createElement("h1")
     newHeader.id= 'victory'
-    newHeader.textContent='YOUR-NAME is the champion'
+    newHeader.textContent='Gray is the champion'
 
     
